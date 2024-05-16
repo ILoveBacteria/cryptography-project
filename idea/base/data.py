464feed80484f85956994f181a02bf971521f756
Moein Arabi <moein.mo81@gmail.com>
2024-05-16 11:40:25 +0330

    Break into sub blocks and joining block

--- idea/base/data.py
+++ idea/base/data.py
@@ -1,7 +1,22 @@
+from typing import Iterable
+
 from idea.base.math import add, multiple, xor
 
 
 class Block:
+    @classmethod
+    def join(cls, blocks:Iterable['Block']) -> 'Block':
+        return Block(b''.join(block.data for block in blocks))
+    
+    def break_into_sub_blocks(self, size:int) -> tuple['Block']:
+        if size < 1:
+            raise ValueError('Size must be greater than 0!')
+        if size > len(self):
+            raise ValueError('Size must be less than the length of the block!')
+        if len(self) % size != 0:
+            raise ValueError('Size must be a divisor of the length of the block!')
+        return tuple(Block(self.data[i:i + size]) for i in range(0, len(self), size))
+
     def __init__(self, data:bytes):
         self.data = data
     
