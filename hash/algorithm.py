b9c9ad3b4dad8f6fa70c8c9560d34b129618422f
Moein Arabi <moein.mo81@gmail.com>
2024-06-06 14:52:20 +0330

    Add sbox and w function

--- hash/algorithm.py
+++ hash/algorithm.py
@@ -1,8 +1,8 @@
-from cryptography.utils import Block
+from hash.utils import Block, S_Box
 
 
 class MyHash:
-    def __init__(self, plain_text:bytes, keys:list[bytes], salt:bytes, work_factor:int) -> None:
+    def __init__(self, plain_text:bytes, keys:list[bytes], salt:bytes, work_factor:int, sbox:list[S_Box]) -> None:
         if len(salt) != 8:
             raise ValueError('Salt must be 8 bytes long')
         if len(plain_text) != 8:
@@ -12,10 +12,13 @@ class MyHash:
         for key in keys:
             if len(key) != 4:
                 raise ValueError('Each key must be 4 bytes long')
+        if len(sbox) != 4:
+            raise ValueError('S-Boxes must be 4')
         self.plain_text = Block(plain_text)
         self.keys = list(map(Block, keys))
         self.salt = Block(salt)
         self.work_factor = work_factor
+        self.sbox = sbox
 
     def encrypt(self) -> bytes:
         cipher_text = self.plain_text
@@ -30,17 +33,15 @@ class MyHash:
         cipher_text = self.final_round(cipher_text, self.keys[i])
         return cipher_text
 
-    def round(self, input:Block, key:Block) -> Block:
-        left, right = input[:4], input[4:]
-        return Block.join(w(left ^ key) ^ right, left)
+    def round(self, input_64:Block, key:Block) -> Block:
+        left_32, right_32 = input_64[:4], input_64[4:]
+        return Block.join(self.w(left_32 ^ key) ^ right_32, left_32)
 
-    def final_round(self, input:Block) -> Block:
-        left, right = input[:4], input[4:]
-        return Block.join(right ^ self.keys[31], left ^ self.keys[30])
+    def final_round(self, input_64:Block) -> Block:
+        left_32, right_32 = input_64[:4], input_64[4:]
+        return Block.join(right_32 ^ self.keys[31], left_32 ^ self.keys[30])
 
-
-def w():
-    pass
-
-
-
+    def w(self, input_32):
+        a = self.sbox[0].substitute(input_32[:1]) + self.sbox[1].substitute(input_32[1:2])
+        b = self.sbox[2].substitute(input_32[2:3]) ^ a
+        return self.sbox[3].substitute(input_32[3:]) + b
